687b6caf58ba39492a84d15849d060f5cfe78c51
Christian BONGWALA <bongwala@freeland.com>
2022-12-07 15:51:03 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import React, { useState, useEffect} from 'react';
+import { commerce } from './lib/commerce';
+import { Navbar, Products} from './components';
+
+const App = () => {
+
+   const [products, setProducts]= useState([]);
+
+   const fetchProducts = async () => {
+         const { data } = await commerce.products.list();
+
+    setProducts(data);
+
+   };
+
+   useEffect (() => {  
+      fetchProducts();
+   }, []);
+
+  
+
+  return (
+    <div>
+     <Navbar />
+     <Products products = { products }/> 
+
+    </div>
+  );
+};
+
+export default App;
+
